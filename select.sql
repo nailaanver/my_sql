790a708caed987aa47205ce90220475ceee2c754
Fathima Naila <fathimanaila71@gmail.com>
2025-11-26 21:36:34 +0530

    complete select queriy task

---
+++ select.sql
@@ -0,0 +1,33 @@
+use HospitalDB;
+
+1--
+select * from patient; 
+
+2--
+select name from doctors; 
+
+3--
+select * from patient where gender = 'female'; 
+
+4--
+select * from patient where city = 'Delhi'; 
+
+5--
+select status from appointments; 
+
+6--
+select name,phone from doctors; 
+
+7--
+select * from patient where age > 30;
+
+8--
+select appointment_date from appointments;  
+
+9--
+select room_type from room;
+
+10--
+select department_name from department; 
+
+ 
